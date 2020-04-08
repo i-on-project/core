89e14ebe393051c55db5003720a0d0eebdd02a31
Jorge Brown <jorge.pc.brown@gmail.com>
2020-04-08 13:57:49 +0100

    Changed Calendar according to the previous change

--- project/src/main/kotlin/org/ionproject/core/calendar/icalendar/Calendar.kt
+++ project/src/main/kotlin/org/ionproject/core/calendar/icalendar/Calendar.kt
@@ -1,21 +1,20 @@
 package org.ionproject.core.calendar.icalendar
 
-import org.ionproject.core.calendar.iCalendarFold
 import org.ionproject.core.calendar.icalendar.properties.calendar.CalendarScale
 import org.ionproject.core.calendar.icalendar.properties.calendar.Method
 import org.ionproject.core.calendar.icalendar.properties.calendar.ProductIdentifier
 import org.ionproject.core.calendar.icalendar.properties.calendar.Version
-import org.ionproject.core.twoPhaseReduce
 
 const val iCalendarVersion = "2.0"
 
 class Calendar (
-    private val prod: ProductIdentifier,
-    private val version: Version = Version(),
-    private val scale: CalendarScale? = null,
-    private val method: Method? = null,
-    private val components: MutableList<CalendarComponent> = mutableListOf()
+    val prod: ProductIdentifier,
+    val version: Version = Version(),
+    val scale: CalendarScale? = null,
+    val method: Method? = null,
+    val components: MutableList<CalendarComponent> = mutableListOf()
 ) : Iterable<CalendarComponent> {
+
     constructor(
         prod: ProductIdentifier,
         version: Version = Version(),
@@ -23,18 +22,6 @@ class Calendar (
         method: Method? = null,
         vararg components: CalendarComponent
     ) : this(prod, version, scale, method, mutableListOf(*components))
-
-    override fun toString(): String {
-        val properties = listOfNotNull(prod, version, scale, method).twoPhaseReduce({ it.toiCalendar().iCalendarFold() },{ acc, it ->
-            "$acc$it"
-        })
-
-        val subComponents = components.twoPhaseReduce({ it.toString() },{ acc, it ->
-            "$acc$it"
-        })
-
-        return "BEGIN:VCALENDAR\r\n${properties}${subComponents}END:VCALENDAR\r\n"
-    }
 
     override fun iterator(): Iterator<CalendarComponent> = components.iterator()
 }
