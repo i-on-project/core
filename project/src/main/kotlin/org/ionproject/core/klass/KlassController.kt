6dbc5083d5ad07c834836636bd1f8fe55fdb120f
João Pereira <47888471+joao-pereira5@users.noreply.github.com>
2020-04-08 11:07:42 +0100

    Class repository, fetching from DB.

--- project/src/main/kotlin/org/ionproject/core/klass/KlassController.kt
+++ project/src/main/kotlin/org/ionproject/core/klass/KlassController.kt
@@ -2,12 +2,18 @@ package org.ionproject.core.klass
 
 import org.ionproject.core.common.Siren
 import org.ionproject.core.common.Uri
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class KlassController(private val repo: KlassRepo) {
+
+    @ExceptionHandler
+    fun handleClassNotInDbException(exception: ClassNotInDbException) =
+        ResponseEntity.notFound().build<Any>()
 
     @GetMapping(Uri.klassByTerm)
     fun get(@PathVariable acr: String, @PathVariable calterm: String): Siren {
@@ -18,6 +24,11 @@ class KlassController(private val repo: KlassRepo) {
     @GetMapping(Uri.klasses)
     fun getCollection(@PathVariable acr: String): Siren {
         val klass = repo.getPage(acr, 0, 3)
+
+        if (klass.isEmpty()) {
+            throw ClassNotInDbException()
+        }
+
         return KlassCollectionOutputModel(acr, klass).toSiren()
     }
 }
--- project/src/main/kotlin/org/ionproject/core/klass/KlassRepo.kt
+++ project/src/main/kotlin/org/ionproject/core/klass/KlassRepo.kt
@@ -1,19 +1,57 @@
 package org.ionproject.core.klass
 
 import org.ionproject.core.classSection.ClassSection
+import org.jdbi.v3.core.Jdbi
+import org.jdbi.v3.sqlobject.SqlObjectPlugin
+import org.jdbi.v3.sqlobject.statement.SqlQuery
+import org.postgresql.ds.PGSimpleDataSource
 import org.springframework.stereotype.Component
+import java.lang.Exception
+
+class ClassNotInDbException : Exception()
 
 @Component
 class KlassRepo {
-    fun get(acr: String, calendarTerm: String): FullKlass =
-        FullKlass(
-                acr,
-                calendarTerm,
-                listOf(ClassSection(acr, calendarTerm, "1d"), ClassSection(acr, calendarTerm, "2d")))
+    fun get(acr: String, calendarTerm: String): FullKlass {
+        val dataSource = PGSimpleDataSource()
+        dataSource.databaseName = System.getenv("CORE_DB_NAME")
+        dataSource.user = System.getenv("CORE_DB_ROLE")
+        //dataSource.password = System.getenv("CORE_DB_PASS")
+        val j = Jdbi.create(dataSource).installPlugin(SqlObjectPlugin()).open()
 
-    fun getPage(acr: String, page: Int, size: Int): List<Klass> =
-        listOf(
-            Klass(acr, "1920v"),
-            Klass(acr, "1920i"),
-            Klass(acr, "1819v"))
+        val acrUpper = acr.toUpperCase()
+
+        val klass = j.createQuery("select CR.acronym, C.term from dbo.Class as C join dbo.Course as CR on C.courseid=CR.id where CR.acronym=:acr and C.term=:term")
+            .bind("acr", acrUpper)
+            .bind("term", calendarTerm)
+            .map { ro, _ -> Klass(ro.getString("acronym"), ro.getString("term")) }
+            .findOne()
+
+        if (klass.isEmpty) {
+            throw ClassNotInDbException()
+        }
+
+        val sections = j.createQuery("select CR.acronym, C.term, CS.id from dbo.Class as C join dbo.ClassSection as CS on C.courseid=CS.courseid and C.term=CS.term join dbo.Course as CR on CR.id=C.courseid where CR.acronym=:acr and C.term=:term;")
+            .bind("acr", acrUpper)
+            .bind("term", calendarTerm)
+            .map { ro, _ -> ClassSection(ro.getString("acronym"), ro.getString("term"), ro.getString("id")) }
+            .list()
+
+        return FullKlass(klass.get().course, klass.get().calendarTerm, sections)
+    }
+
+    fun getPage(acr: String, page: Int, size: Int): List<Klass> {
+        val dataSource = PGSimpleDataSource()
+        dataSource.databaseName = "core" //System.getenv("CORE_DB_NAME")
+        dataSource.user = "bob" //System.getenv("CORE_DB_ROLE")
+        //dataSource.password = //System.getenv("CORE_DB_PASS")
+        val j = Jdbi.create(dataSource).installPlugin(SqlObjectPlugin()).open()
+
+        val acrUpper = acr.toUpperCase()
+
+        return j.createQuery("select CR.acronym, C.term from dbo.Class as C join dbo.Course as CR on C.courseid=CR.id where CR.acronym=:acr;")
+            .bind("acr", acrUpper)
+            .map { ro, _ -> Klass(ro.getString("acronym"), ro.getString("term")) }
+            .list()
+    }
 }
