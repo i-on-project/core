08a6f339c4d1856f962bd5c9afd5943ebe23d27e
AlexP <pena.alex49@hotmail.com>
2020-04-13 16:52:01 -0400

    finish review

--- sql/script_sql_testDb.sh
+++ sql/script_sql_testDb.sh
@@ -1,6 +1,8 @@
 echo "Droping Tables"
-psql  -U postgres -d I-ON-TEST -f drop_tables.sql
+psql  -U postgres -1 -d I-ON -f drop.sql
 echo "Creating Tables"
-psql  -U postgres -d I-ON-TEST -f create.sql
+psql  -U postgres -1 -d I-ON -f create.sql
 echo "Inserting Data in Tables"
-psql  -U postgres -d I-ON-TEST -f insert.sql
+psql  -U postgres -1 -d I-ON -f insert.sql
+echo "Creating views"
+psql  -U postgres -1 -d I-ON -f course_scripts.sql
