d08c9c099625004afc1d480bcb902318c3adaa98
Jorge Brown <jorge.pc.brown@gmail.com>
2020-04-27 17:07:38 +0100

    Views for calendar components done

--- sql/calendar.sql
+++ sql/calendar.sql
@@ -1,14 +1,14 @@
-CREATE TABLE dbo.ICalendarDataType (
+CREATE TABLE IF NOT EXISTS dbo.ICalendarDataType (
     id             INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name           VARCHAR(16) NOT NULL UNIQUE
 );
 
-CREATE TABLE dbo.Language(
+CREATE TABLE IF NOT EXISTS dbo.Language(
     id             INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name           VARCHAR(64) NOT NULL UNIQUE
 );
 
-CREATE TABLE dbo.Category(
+CREATE TABLE IF NOT EXISTS dbo.Category(
 	id             INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name           VARCHAR(64) NOT NULL,
 	language       INT REFERENCES dbo.Language(id),
@@ -16,42 +16,42 @@ CREATE TABLE dbo.Category(
 );
 
 -- some iCal property types
-CREATE TABLE dbo.Description (
+CREATE TABLE IF NOT EXISTS dbo.Description (
     comp_id        INT REFERENCES dbo.CalendarComponent(id),
     value          VARCHAR(200),
     language       INT REFERENCES dbo.Language(id)
 );
 
-CREATE TABLE dbo.Summary (
+CREATE TABLE IF NOT EXISTS dbo.Summary (
     comp_id        INT REFERENCES dbo.CalendarComponent(id),
     value          VARCHAR(50) NOT NULL,
     language       INT REFERENCES dbo.Language(id)
 );
 
-CREATE TABLE dbo.Attachment (
+CREATE TABLE IF NOT EXISTS dbo.Attachment (
     comp_id        INT REFERENCES dbo.CalendarComponent(id),
     value          VARCHAR(128) NOT NULL
 );
 
-CREATE TABLE dbo.Due (
+CREATE TABLE IF NOT EXISTS dbo.Due (
     comp_id        INT REFERENCES dbo.CalendarComponent(id),
     type           INT REFERENCES dbo.ICalendarDataType(id),
     value          TIMESTAMP
 );
 
-CREATE TABLE dbo.Dtend (
+CREATE TABLE IF NOT EXISTS dbo.Dtend (
     comp_id        INT REFERENCES dbo.CalendarComponent(id),
     type           INT REFERENCES dbo.ICalendarDataType(id),
     value          TIMESTAMP
 );
 
-CREATE TABLE dbo.Dtstart (
+CREATE TABLE IF NOT EXISTS dbo.Dtstart (
     comp_id        INT REFERENCES dbo.CalendarComponent(id),
     type           INT REFERENCES dbo.ICalendarDataType(id),
     value          TIMESTAMP
 );
 
-CREATE TABLE dbo.Categories (
+CREATE TABLE IF NOT EXISTS dbo.Categories (
     comp_id        INT REFERENCES dbo.CalendarComponent(id),
     value          INT REFERENCES dbo.Category(id)
 );
@@ -59,82 +59,113 @@ CREATE TABLE dbo.Categories (
 ---- for easier queries OR Calendar Components
 ---- these views will retrieve all the relevant Calendar Property types for a given calendar component
 -- VTODO
-CREATE OR REPLACE view dbo.v_Todo AS 
-	SELECT DISTINCT
-        CalComp.calendar_id,
-        Comp.id AS uid,
-        Summ.value AS summary,
-        Summ.language AS summary_language,
-        Descr.value AS description,
-        Descr.language AS description_language,
-        Att.value AS attachment,
-        Comp.dtstamp,
-        Comp.created,
-        D.value AS due,
-        D.type AS due_value_data_type,
-        Cat.value as category
-	FROM dbo.CalendarComponent AS Comp
-    JOIN dbo.CalendarComponents AS CalComp ON CalComp.comp_id=Comp.id
-    JOIN dbo.Summary AS Summ ON Comp.id = Summ.comp_id
-    JOIN dbo.Description AS Descr ON Comp.id = Descr.comp_id
-    JOIN dbo.Categories AS Cat ON Comp.id = Cat.comp_id
-    JOIN dbo.Due AS D ON D.comp_id=Comp.id
-    JOIN dbo.Attachment AS Att ON Comp.id=Att.comp_id
-    WHERE Comp.type = 'T';
-
--- VEVENT
-CREATE OR REPLACE view dbo.v_Event AS 
-	SELECT DISTINCT
-        CalComp.calendar_id,
-        Comp.id AS uid,
-        Summ.value AS summary,
-        Summ.language AS summary_language,
-        Descr.value AS description,
-        Descr.language AS description_language,
-        Comp.dtstamp,
-        Comp.created,
-        Cat.value as category,
-        DS.value as dtstart,
-        DS.type as dtstart_value_data_type,
-        DE.value AS dtend,
-        DE.type AS dtend_value_data_type,
-        RR.byday
-	FROM dbo.CalendarComponent AS Comp
-    JOIN dbo.CalendarComponents AS CalComp ON CalComp.comp_id=Comp.id
-    JOIN dbo.Summary AS Summ ON Comp.id = Summ.comp_id
-    JOIN dbo.Description AS Descr ON Comp.id = Descr.comp_id
-    JOIN dbo.Categories AS Cat ON Comp.id = Cat.comp_id
-    JOIN dbo.Dtstart as DS ON Comp.id = DS.comp_id
-    JOIN dbo.Dtend AS DE ON Comp.id=DE.comp_id
-    JOIN dbo.RecurrenceRule AS RR ON Comp.id=RR.comp_id
-    WHERE Comp.type = 'E';
-
-DROP view dbo.v_Journal;
--- VJOURNAL
-CREATE OR REPLACE view dbo.v_Journal AS 
+DROP VIEW dbo.v_Todo;
+CREATE OR REPLACE VIEW dbo.v_Todo AS 
     SELECT DISTINCT
 		Comp.id AS uid,
 		ARRAY_AGG(DISTINCT CalComp.calendar_id) AS calendars,
-		ARRAY_AGG(DISTINCT Summ.value) AS summaries,
-        ARRAY_AGG(DISTINCT Summ.language) AS summary_language,
-        ARRAY_AGG(DISTINCT Descr.value) AS description,
-        ARRAY_AGG(DISTINCT Descr.language) AS description_language,
+		ARRAY_AGG(DISTINCT MERGE_LANGUAGE_TEXT(Summ.language, Summ.value)) AS summaries,
+        ARRAY_AGG(DISTINCT MERGE_LANGUAGE_TEXT(Descr.language, Descr.value)) AS descriptions,
+        Comp.dtstamp,
+        Comp.created,
+        ARRAY_AGG(DISTINCT Cat.value) AS categories,
+		ARRAY_AGG(DISTINCT Att.value) AS attachments,
+		D.value AS due,
+        D.type AS due_value_data_type
+    FROM 
+		dbo.CalendarComponent AS Comp
+    JOIN 
+		dbo.CalendarComponents AS CalComp ON CalComp.comp_id=Comp.id
+    JOIN 
+		dbo.Summary AS Summ ON Comp.id = Summ.comp_id
+    JOIN 
+		dbo.Description AS Descr ON Comp.id = Descr.comp_id
+    JOIN 
+		dbo.Categories AS Cat ON Comp.id = Cat.comp_id
+	JOIN 
+		dbo.Attachment AS Att ON Comp.id = Att.comp_id
+	JOIN
+		dbo.Due AS D ON Comp.id=D.comp_id
+    WHERE 
+		Comp.type = 'T'
+	GROUP BY
+		uid, due, due_value_data_type
+	ORDER BY 
+		uid;
+
+-- VEVENT
+DROP VIEW dbo.v_Event;
+CREATE OR REPLACE VIEW dbo.v_Event AS 
+    SELECT DISTINCT
+		Comp.id AS uid,
+		ARRAY_AGG(DISTINCT CalComp.calendar_id) AS calendars,
+		ARRAY_AGG(DISTINCT MERGE_LANGUAGE_TEXT(Summ.language, Summ.value)) AS summaries,
+        ARRAY_AGG(DISTINCT MERGE_LANGUAGE_TEXT(Descr.language, Descr.value)) AS descriptions,
+        Comp.dtstamp,
+        Comp.created,
+        ARRAY_AGG(DISTINCT Cat.value) AS categories,
+        DS.value AS dtstart,
+        DS.type AS dtstart_value_data_type,
+		DE.value AS dtend,
+        DE.type AS dtend_value_data_type,
+		ARRAY_AGG(DISTINCT RR.byday)
+    FROM 
+		dbo.CalendarComponent AS Comp
+    JOIN 
+		dbo.CalendarComponents AS CalComp ON CalComp.comp_id=Comp.id
+    JOIN 
+		dbo.Summary AS Summ ON Comp.id = Summ.comp_id
+    JOIN 
+		dbo.Description AS Descr ON Comp.id = Descr.comp_id
+    JOIN 
+		dbo.Categories AS Cat ON Comp.id = Cat.comp_id
+    JOIN 
+		dbo.Dtstart as DS ON Comp.id = DS.comp_id
+	JOIN
+		dbo.Dtend AS DE ON Comp.id=DE.comp_id
+    JOIN
+		dbo.RecurrenceRule AS RR ON Comp.id=RR.comp_id
+    WHERE 
+		Comp.type = 'E'
+	GROUP BY
+		uid, dtstart, dtstart_value_data_type, dtend, dtend_value_data_type
+	ORDER BY 
+		uid;
+
+-- VJOURNAL
+DROP VIEW dbo.v_Journal;
+CREATE OR REPLACE VIEW dbo.v_Journal AS 
+    SELECT DISTINCT
+		Comp.id AS uid,
+		ARRAY_AGG(DISTINCT CalComp.calendar_id) AS calendars,
+		ARRAY_AGG(DISTINCT MERGE_LANGUAGE_TEXT(Summ.language, Summ.value)) AS summaries,
+        ARRAY_AGG(DISTINCT MERGE_LANGUAGE_TEXT(Descr.language, Descr.value)) AS descriptions,
         ARRAY_AGG(DISTINCT Att.value) AS attachments,
         Comp.dtstamp,
         Comp.created,
-        ARRAY_AGG(DISTINCT Cat.value) as categories,
-        DS.value as dtstart,
-        DS.type as dtstart_value_data_type
-    FROM dbo.CalendarComponent AS Comp
-    JOIN dbo.CalendarComponents AS CalComp ON CalComp.comp_id=Comp.id
-    JOIN dbo.Summary AS Summ ON Comp.id = Summ.comp_id
-    JOIN dbo.Description AS Descr ON Comp.id = Descr.comp_id
-    JOIN dbo.Attachment AS Att ON Comp.id = Att.comp_id
-    JOIN dbo.Categories AS Cat ON Comp.id = Cat.comp_id
-    JOIN dbo.Dtstart as DS ON Comp.id = DS.comp_id
-    WHERE Comp.type = 'J'
-	GROUP BY uid, dtstart, dtstart_value_data_type
-	ORDER BY uid;
+        ARRAY_AGG(DISTINCT Cat.value) AS categories,
+        DS.value AS dtstart,
+        DS.type AS dtstart_value_data_type
+    FROM 
+		dbo.CalendarComponent AS Comp
+    JOIN 
+		dbo.CalendarComponents AS CalComp ON CalComp.comp_id=Comp.id
+    JOIN 
+		dbo.Summary AS Summ ON Comp.id = Summ.comp_id
+    JOIN 
+		dbo.Description AS Descr ON Comp.id = Descr.comp_id
+    JOIN 
+		dbo.Attachment AS Att ON Comp.id = Att.comp_id
+    JOIN 
+		dbo.Categories AS Cat ON Comp.id = Cat.comp_id
+    JOIN 
+		dbo.Dtstart as DS ON Comp.id = DS.comp_id
+    WHERE 
+		Comp.type = 'J'
+	GROUP BY
+		uid, dtstart, dtstart_value_data_type
+	ORDER BY 
+		uid;
 
 ---- for creation of calendar components
 ---- these will verify constraints and insert in the appropriate tables
@@ -146,7 +177,7 @@ CREATE OR REPLACE PROCEDURE dbo.newEvent(
 	category INT,
 	dtstart TIMESTAMP,
     dtend TIMESTAMP,
-    dtstart_dtend_type INT REFERENCES dbo.ICalendarDataType(id),
+    dtstart_dtend_type INT,
     byday VARCHAR(20)
 ) AS $$
 #print_strict_params ON
