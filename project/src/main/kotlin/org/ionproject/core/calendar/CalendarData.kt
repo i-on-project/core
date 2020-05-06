a1c253d7dcc156df6b6327b28ccb876c34f2adbd
Jorge Brown <jorge.pc.brown@gmail.com>
2020-05-06 18:36:46 +0100

    Still trying to improve CalendarData readability

--- project/src/main/kotlin/org/ionproject/core/calendar/CalendarData.kt
+++ project/src/main/kotlin/org/ionproject/core/calendar/CalendarData.kt
@@ -26,137 +26,125 @@ import java.time.OffsetDateTime
 import org.ionproject.core.calendar.icalendar.types.Date as DateType
 
 object CalendarData {
-    private const val CALENDAR_COMPONENT_ABBREVIATION = "cmp"
-    private const val CALENDAR_COMPONENT = "dbo.v_ComponentsAll $CALENDAR_COMPONENT_ABBREVIATION"
+    // Name of Calendar component view and of calendar property columns
+    private const val CALENDAR_COMPONENT = "dbo.v_ComponentsAll"
+    private const val UID = "uid"
+    private const val CALENDARS = "calendars"
+    private const val TYPE = "type"
+    private const val SUMMARIES = "summaries"
+    private const val SUMMARIES_LANGUAGE = "summaries_language"
+    private const val DESCRIPTIONS = "descriptions"
+    private const val DESCRIPTIONS_LANGUAGE = "descriptions_language"
+    private const val CATEGORIES = "categories"
+    private const val DTSTAMP = "dtstamp"
+    private const val CREATED = "created"
+    private const val ATTACHMENTS = "attachments"
+    private const val DTSTART = "dtstart"
+    private const val DTEND = "dtend"
+    private const val BYDAY = "byday"
+    private const val DUE = "due"
 
-    private const val UID_COLUMN = "uid"
-    private const val CALENDARS_COLUMN = "calendars"
-    private const val TYPE_COLUMN = "type"
-    private const val SUMMARIES_COLUMN = "summaries"
-    private const val SUMMARIES_LANGUAGE_COLUMN = "summaries_language"
-    private const val DESCRIPTIONS_COLUMN = "descriptions"
-    private const val DESCRIPTIONS_LANGUAGE_COLUMN = "descriptions_language"
-    private const val CATEGORIES_COLUMN = "categories"
-    private const val DTSTAMP_COLUMN = "dtstamp"
-    private const val CREATED_COLUMN = "created"
-    private const val ATTACHMENTS_COLUMN = "attachments"
-    private const val DTSTART_COLUMN = "dtstart"
-    private const val DTEND_COLUMN = "dtend"
-    private const val BYDAY_COLUMN = "byday"
-    private const val DUE_COLUMN = "due"
+    // Class and ClassSection table and column names
+    private const val CLASS = "dbo.Class"
+    private const val CLASS_SECTION = "dbo.ClassSection"
+    private const val CALENDAR = "calendar"
+    private const val COURSE = "courseId"
+    private const val TERM = "term"
+    private const val ID = "id"
 
-    private const val UID = "$CALENDAR_COMPONENT_ABBREVIATION.$UID_COLUMN"
-    private const val CALENDARS = "$CALENDAR_COMPONENT_ABBREVIATION.$CALENDARS_COLUMN"
-    private const val TYPE = "$CALENDAR_COMPONENT_ABBREVIATION.$TYPE_COLUMN"
-    private const val SUMMARIES = "$CALENDAR_COMPONENT_ABBREVIATION.$SUMMARIES_COLUMN"
-    private const val SUMMARIES_LANGUAGE = "$CALENDAR_COMPONENT_ABBREVIATION.$SUMMARIES_LANGUAGE_COLUMN"
-    private const val DESCRIPTIONS = "$CALENDAR_COMPONENT_ABBREVIATION.$DESCRIPTIONS_COLUMN"
-    private const val DESCRIPTIONS_LANGUAGE = "$CALENDAR_COMPONENT_ABBREVIATION.$DESCRIPTIONS_LANGUAGE_COLUMN"
-    private const val CATEGORIES = "$CALENDAR_COMPONENT_ABBREVIATION.$CATEGORIES_COLUMN"
-    private const val DTSTAMP = "$CALENDAR_COMPONENT_ABBREVIATION.$DTSTAMP_COLUMN"
-    private const val CREATED = "$CALENDAR_COMPONENT_ABBREVIATION.$CREATED_COLUMN"
-    private const val ATTACHMENTS = "$CALENDAR_COMPONENT_ABBREVIATION.$ATTACHMENTS_COLUMN"
-    private const val DTSTART = "$CALENDAR_COMPONENT_ABBREVIATION.$DTSTART_COLUMN"
-    private const val DTEND = "$CALENDAR_COMPONENT_ABBREVIATION.$DTEND_COLUMN"
-    private const val BYDAY = "$CALENDAR_COMPONENT_ABBREVIATION.$BYDAY_COLUMN"
-    private const val DUE = "$CALENDAR_COMPONENT_ABBREVIATION.$DUE_COLUMN"
-
+    // Desired columns from the $CALENDAR_COMPONENT table/view when querying to get desired mapping functionality
     private const val SELECT = """
     select 
-        $UID,
-        $TYPE,
-        array_agg(distinct merge_language_text($SUMMARIES_LANGUAGE, $SUMMARIES)) as $SUMMARIES_COLUMN,
-        array_agg(distinct merge_language_text($DESCRIPTIONS_LANGUAGE, $DESCRIPTIONS)) as $DESCRIPTIONS_COLUMN,
-        array_agg(distinct $CATEGORIES) as $CATEGORIES_COLUMN,
-        array_agg(distinct $ATTACHMENTS) as $ATTACHMENTS_COLUMN,
-        $DTSTAMP,
-        $CREATED,
-        $DTSTART,
-        $DTEND,
-        $DUE,
-        $BYDAY
+        $CALENDAR_COMPONENT.$UID,
+        $CALENDAR_COMPONENT.$TYPE,
+        array_agg(distinct merge_language_text($CALENDAR_COMPONENT.$SUMMARIES_LANGUAGE, $CALENDAR_COMPONENT.$SUMMARIES)) as $SUMMARIES,
+        array_agg(distinct merge_language_text($CALENDAR_COMPONENT.$DESCRIPTIONS_LANGUAGE, $CALENDAR_COMPONENT.$DESCRIPTIONS)) as $DESCRIPTIONS,
+        array_agg(distinct $CALENDAR_COMPONENT.$CATEGORIES) as $CATEGORIES,
+        array_agg(distinct $CALENDAR_COMPONENT.$ATTACHMENTS) as $ATTACHMENTS,
+        $CALENDAR_COMPONENT.$DTSTAMP,
+        $CALENDAR_COMPONENT.$CREATED,
+        $CALENDAR_COMPONENT.$DTSTART,
+        $CALENDAR_COMPONENT.$DTEND,
+        $CALENDAR_COMPONENT.$DUE,
+        $CALENDAR_COMPONENT.$BYDAY
     """
 
+    // This group by is necessary when queries use $SELECT, otherwise the use of array_agg will launch an SQL exception
     private const val GROUP_BY = """group by
-        $UID, 
-        $TYPE,
-        $DTSTAMP,
-        $CREATED,
-        $DTSTART,
-        $DTEND,
-        $DUE,
-        $BYDAY"""
+        $CALENDAR_COMPONENT.$UID, 
+        $CALENDAR_COMPONENT.$TYPE,
+        $CALENDAR_COMPONENT.$DTSTAMP,
+        $CALENDAR_COMPONENT.$CREATED,
+        $CALENDAR_COMPONENT.$DTSTART,
+        $CALENDAR_COMPONENT.$DTEND,
+        $CALENDAR_COMPONENT.$DUE,
+        $CALENDAR_COMPONENT.$BYDAY"""
 
-    private const val CLASS_ABBREVIATION = "c"
-    private const val CLASS = "dbo.Class $CLASS_ABBREVIATION"
-
-    private const val CALENDAR_ID_COLUMN = "calendar"
-    private const val COURSE_COLUMN = "courseId"
-    private const val TERM_COLUMN = "term"
-    private const val ID_COLUMN = "id"
-
-    private const val CLASS_CALENDAR_ID = "$CLASS_ABBREVIATION.$CALENDAR_ID_COLUMN"
-    private const val CLASS_COURSE = "$CLASS_ABBREVIATION.$COURSE_COLUMN"
-    private const val CLASS_TERM = "$CLASS_ABBREVIATION.$TERM_COLUMN"
-
-    private const val CLASS_SECTION_ABBREVIATION = "cs"
-    private const val CLASS_SECTION = "dbo.Class $CLASS_SECTION_ABBREVIATION"
-
-    private const val CLASS_SECTION_CALENDAR_ID = "$CLASS_SECTION_ABBREVIATION.$CALENDAR_ID_COLUMN"
-    private const val CLASS_SECTION_ID = "$CLASS_SECTION_ABBREVIATION.$ID_COLUMN"
-    private const val CLASS_SECTION_COURSE = "$CLASS_SECTION_ABBREVIATION.$COURSE_COLUMN"
-    private const val CLASS_SECTION_TERM = "$CLASS_SECTION_ABBREVIATION.$TERM_COLUMN"
-
-    const val CALENDAR_FROM_CLASS_QUERY = """with calendar_id as (
-                    select $CLASS_CALENDAR_ID
+    const val CALENDAR_FROM_CLASS_QUERY =
+        """with calendar_id as (
+                    select $CLASS.$CALENDAR
                     from $CLASS
-                    where $CLASS_COURSE = :$COURSE_COLUMN and $CLASS_TERM = :$TERM_COLUMN
+                    where $CLASS.$COURSE = :$COURSE and $CLASS.$TERM = :$TERM
                 )
             $SELECT
             from
                 $CALENDAR_COMPONENT
-            where $CALENDARS = (select * from calendar_id)
+            where 
+                $CALENDAR_COMPONENT.$CALENDARS = (select * from calendar_id) 
             $GROUP_BY"""
 
-    const val CALENDAR_COMPONENT_FROM_CLASS_QUERY = """with calendar_id as (
-                    select $CLASS_CALENDAR_ID
+    const val CALENDAR_COMPONENT_FROM_CLASS_QUERY =
+        """with calendar_id as (
+                    select $CLASS.$CALENDAR
                     from $CLASS
-                    where $CLASS_COURSE = :$COURSE_COLUMN and $CLASS_TERM = :$TERM_COLUMN
+                    where $CLASS.$COURSE = :$COURSE and $CLASS.$TERM = :$TERM
                 )
             $SELECT
-            from $CALENDAR_COMPONENT
-            where $CALENDARS = (select * from calendar_id) and $UID = :$UID_COLUMN
+            from 
+                $CALENDAR_COMPONENT
+            where 
+                $CALENDAR_COMPONENT.$CALENDARS = (select * from calendar_id)
+                and
+                $CALENDAR_COMPONENT.$UID = :$UID
             $GROUP_BY"""
 
     const val CALENDAR_FROM_CLASS_SECTION_QUERY =
         """with calendar_id as (
-                    select $CLASS_SECTION_CALENDAR_ID
-                    from $CLASS_SECTION
+                    select 
+                        $CLASS_SECTION.$CALENDAR
+                    from 
+                        $CLASS_SECTION
                     where 
-                        $CLASS_SECTION_COURSE = :$COURSE_COLUMN 
+                        $CLASS_SECTION.$COURSE = :$COURSE 
                         and
-                        $CLASS_SECTION_TERM = :$TERM_COLUMN
+                        $CLASS_SECTION.$TERM = :$TERM
                         and
-                        $CLASS_SECTION_ID = :$ID_COLUMN
+                        $CLASS_SECTION.$ID = :$ID
                 )
             $SELECT
             from $CALENDAR_COMPONENT
             where $CALENDARS = (select * from calendar_id)
             $GROUP_BY"""
 
-    const val CALENDAR_COMPONENT_FROM_CLASS_SECTION_QUERY = """with calendar_id as (
-                    select $CLASS_SECTION_CALENDAR_ID
-                    from $CLASS_SECTION
+    const val CALENDAR_COMPONENT_FROM_CLASS_SECTION_QUERY =
+        """with calendar_id as (
+                    select 
+                        $CLASS_SECTION.$CALENDAR
+                    from 
+                        $CLASS_SECTION
                     where 
-                        $CLASS_SECTION_COURSE = :$COURSE_COLUMN 
+                        $CLASS_SECTION.$COURSE = :$COURSE 
                         and
-                        $CLASS_SECTION_TERM = :$TERM_COLUMN
+                        $CLASS_SECTION.$TERM = :$TERM
                         and
-                        $CLASS_SECTION_ID = :$ID_COLUMN
+                        $CLASS_SECTION.$ID = :$ID
                 )
             $SELECT
             from $CALENDAR_COMPONENT
-            where $CALENDARS = (select * from calendar_id) and $UID = :$UID_COLUMN
+            where
+                $CALENDAR_COMPONENT.$CALENDARS = (select * from calendar_id)
+                and
+                $CALENDAR_COMPONENT.$UID = :$UID
             $GROUP_BY"""
 
     @Component
@@ -166,13 +154,13 @@ object CalendarData {
     ) : RowMapper<CalendarComponent> {
 
         override fun map(rs: ResultSet, ctx: StatementContext): CalendarComponent {
-            val type = rs.getString(TYPE_COLUMN)
-            val uid = UniqueIdentifier(rs.getInt(UID_COLUMN).toString(16)) // TODO(use constant for uid radix)
-            val categories = rs.getCategories(CATEGORIES_COLUMN)
-            val summary = rs.getSummaries(SUMMARIES_COLUMN)
-            val description = rs.getDescriptions(DESCRIPTIONS_COLUMN)
-            val dtStamp = DateTimeStamp(rs.getDatetime(DTSTAMP_COLUMN))
-            val created = DateTimeCreated(rs.getDatetime(CREATED_COLUMN))
+            val type = rs.getString(TYPE)
+            val uid = UniqueIdentifier(rs.getInt(UID).toString(16)) // TODO(use constant for uid radix)
+            val categories = rs.getCategories(CATEGORIES)
+            val summary = rs.getSummaries(SUMMARIES)
+            val description = rs.getDescriptions(DESCRIPTIONS)
+            val dtStamp = DateTimeStamp(rs.getDatetime(DTSTAMP))
+            val created = DateTimeCreated(rs.getDatetime(CREATED))
 
             return when (type) {
                 "E" -> Event(
@@ -182,17 +170,17 @@ object CalendarData {
                     dtStamp,
                     created,
                     categories,
-                    DateTimeStart(rs.getDatetime(DTSTART_COLUMN)),
-                    DateTimeEnd(rs.getDatetime(DTEND_COLUMN)),
-                    rs.getRecurrenceRule(BYDAY_COLUMN)
+                    DateTimeStart(rs.getDatetime(DTSTART)),
+                    DateTimeEnd(rs.getDatetime(DTEND)),
+                    rs.getRecurrenceRule(BYDAY)
                 )
                 "J" -> Journal(
                     uid,
                     summary,
                     description,
-                    rs.getAttachments(ATTACHMENTS_COLUMN),
+                    rs.getAttachments(ATTACHMENTS),
                     dtStamp,
-                    DateTimeStart(rs.getDatetime(DTSTART_COLUMN)),
+                    DateTimeStart(rs.getDatetime(DTSTART)),
                     created,
                     categories
                 )
@@ -200,10 +188,10 @@ object CalendarData {
                     uid,
                     summary,
                     description,
-                    rs.getAttachments(ATTACHMENTS_COLUMN),
+                    rs.getAttachments(ATTACHMENTS),
                     dtStamp,
                     created,
-                    DateTimeDue(rs.getDatetime(DUE_COLUMN)),
+                    DateTimeDue(rs.getDatetime(DUE)),
                     categories
                 )
                 else -> throw UnknownCalendarComponentTypeException("The type represented by $type is not known.")
