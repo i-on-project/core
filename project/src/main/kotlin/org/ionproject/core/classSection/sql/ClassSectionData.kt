125bb81e4631997be187ae05f16b2923ea0e7d16
João Pereira <47888471+joao-pereira5@users.noreply.github.com>
2020-05-13 10:53:13 +0100

    Updated the SQL scripts and adjusted some Repositories
    The queries used in the Repositories had to be changed to match the
    physical model's changes.

--- project/src/main/kotlin/org/ionproject/core/classSection/sql/ClassSectionData.kt
+++ project/src/main/kotlin/org/ionproject/core/classSection/sql/ClassSectionData.kt
@@ -5,19 +5,24 @@ internal object ClassSectionData {
     const val CLASS_SECTION = "classSection"
 
     const val ID = "id"
-    const val CID = "courseid"
-    const val ACR = "acronym"
-    const val CAL_TERM = "term"
+    const val PARENT_CLASS_ID = "classid"
 
     const val COURSE = "course"
     const val COURSE_ID = "id"
     const val COURSE_ACR = "acronym"
 
+    const val CLASS = "class"
+    const val CLASS_ID = "id"
+    const val CID = "courseid"
+    const val ACR = "acronym"
+    const val CAL_TERM = "calendarterm"
+
     const val CLASS_SECTION_QUERY = """
-       select CR.$COURSE_ID as $CID, CR.$COURSE_ACR, CS.$CAL_TERM, CS.$ID
+       select CR.$COURSE_ID as $CID, CR.$COURSE_ACR, C.$CAL_TERM, CS.$ID
        from $SCHEMA.$CLASS_SECTION as CS
-       join $SCHEMA.$COURSE as CR on CS.$CID=CR.$COURSE_ID
-       where CR.$COURSE_ID=:$CID and term=:$CAL_TERM and CS.$ID=:$ID 
+       join $SCHEMA.$CLASS as C on C.$CLASS_ID=CS.$PARENT_CLASS_ID
+       join $SCHEMA.$COURSE as CR on C.$CID=CR.$COURSE_ID
+       where CR.$COURSE_ID=:$CID and $CAL_TERM=:$CAL_TERM and CS.$ID=:$ID 
     """
 }
 
