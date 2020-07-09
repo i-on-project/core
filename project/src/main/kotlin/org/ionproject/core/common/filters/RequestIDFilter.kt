520b42e0bc787dbcd6a6adc04e8bd6227104f5a1
Jorge Brown <jorge.pc.brown@gmail.com>
2020-07-09 16:47:22 +0100

    Added unique IDs to every request.

---
+++ project/src/main/kotlin/org/ionproject/core/common/filters/RequestIDFilter.kt
@@ -0,0 +1,44 @@
+package org.ionproject.core.common.filters
+
+import org.slf4j.MDC // if we use logback switch the import. The method name is the same so everything should work accordingly
+import org.springframework.boot.web.servlet.FilterRegistrationBean
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.filter.OncePerRequestFilter
+import java.util.*
+import javax.servlet.FilterChain
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+private const val REQUEST_ID_HEADER = "Request-Id"
+const val REQUEST_ID = "req-id"
+
+class RequestIDFilter : OncePerRequestFilter() {
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain
+    ) {
+        val requestId = UUID.randomUUID().toString()
+
+        MDC.put(REQUEST_ID, requestId)
+
+        response.addHeader(
+            REQUEST_ID_HEADER,
+            requestId
+        )
+
+        filterChain.doFilter(request, response)
+    }
+}
+
+@Configuration
+class FilterRegistration {
+
+    @Bean
+    fun register() : FilterRegistrationBean<RequestIDFilter> =
+        FilterRegistrationBean<RequestIDFilter>().apply {
+            filter = RequestIDFilter()
+            order = 1
+        }
+}
