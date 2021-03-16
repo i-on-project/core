183af5a2601cd30e7bf73c887db3318646fde788
Ricardo Margalhau <suchxploit@gmail.com>
2021-03-16 23:24:45 +0000

    removes services classes, except for AccessServices

--- project/src/main/kotlin/org/ionproject/core/programme/ProgrammeController.kt
+++ project/src/main/kotlin/org/ionproject/core/programme/ProgrammeController.kt
@@ -13,12 +13,12 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class ProgrammeController(private val programmeServices: ProgrammeServices) {
+class ProgrammeController(private val repo: ProgrammeRepoImpl) {
 
     @ResourceIdentifierAnnotation(ResourceIds.GET_PROGRAMMES, ResourceIds.VERSION)
     @GetMapping(Uri.programmes)
     fun getProgrammes(): ResponseEntity<Siren> {
-        val programmes = programmeServices.getProgrammes()
+        val programmes = repo.getProgrammes()
 
         return ResponseEntity.ok(programmes.programmesListRepr())
     }
@@ -26,7 +26,7 @@ class ProgrammeController(private val programmeServices: ProgrammeServices) {
     @ResourceIdentifierAnnotation(ResourceIds.GET_PROGRAMME, ResourceIds.VERSION)
     @GetMapping(Uri.programmesById)
     fun getProgramme(@PathVariable id: Int): ResponseEntity<Siren> {
-        val programme = programmeServices.getProgrammeById(id)
+        val programme = repo.getProgrammeById(id)
 
         programme?.let { return ResponseEntity.ok(it.programmeToDetailRepr()) }
         return ResponseEntity.notFound().build()
@@ -35,7 +35,7 @@ class ProgrammeController(private val programmeServices: ProgrammeServices) {
     @ResourceIdentifierAnnotation(ResourceIds.GET_OFFER, ResourceIds.VERSION)
     @GetMapping(Uri.programmeOfferById)
     fun getOffer(@PathVariable idProgramme: Int, @PathVariable idOffer: Int): ResponseEntity<Siren> {
-        val offer = programmeServices.getOfferById(idProgramme, idOffer)
+        val offer = repo.getOfferById(idProgramme, idOffer)
 
         offer?.let { return ResponseEntity.ok(it.offerToDetailRepr()) }
         return ResponseEntity.notFound().build()
