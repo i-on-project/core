c57cfdd7ba5018c60f20e06f6662c5a838a3e003
Ricardo Margalhau <suchxploit@gmail.com>
2021-05-25 03:10:32 +0100

    Adds support for React effect cancellation

--- core-react/src/main/Utils.tsx
+++ core-react/src/main/Utils.tsx
@@ -1,4 +1,42 @@
-export const fetchEndpoint = async (endpoint: string, init?: RequestInit): Promise<Response> => {
+export interface CancellableRequest {
+    isCancelled(): boolean
+    request(): Promise<Response>
+    cancel(): void
+}
+
+export const newCancellableRequest = (endpoint: string, init: RequestInit = {}): CancellableRequest => {
+    const abortController = new AbortController()
+    init.signal = abortController.signal
+
+    let futureResponse: Promise<Response> | undefined
+    let cancelled = false
+
+    return {
+        isCancelled: () => cancelled,
+        request: () => {
+            if (futureResponse)
+                return futureResponse
+
+            const request = fetchEndpoint(endpoint, init)
+            futureResponse = request.then(resp => {
+                if (cancelled)
+                    throw new Error('The request has been cancelled')
+
+                return resp
+            })
+
+            return futureResponse
+        },
+        cancel: () => {
+            if (!cancelled && futureResponse) {
+                cancelled = true
+                abortController.abort()
+            }
+        }
+    }
+}
+
+const fetchEndpoint = async (endpoint: string, init?: RequestInit): Promise<Response> => {
     const resp = await fetch(endpoint, init)
         .catch(_ => Promise.reject(new Error('Unexpected Error')))
 
--- core-react/src/main/components/button/SuccessButton.tsx
+++ core-react/src/main/components/button/SuccessButton.tsx
@@ -6,6 +6,7 @@ const SuccessButton = React.forwardRef<HTMLButtonElement, ButtonProps>((props, r
         <button 
         type={ props.type }
         ref={ ref }
+        disabled={ props.disabled } 
         className="px-10 py-3 bg-green-600 hover:bg-green-700 focus:bg-green-700 disabled:opacity-50 disabled:cursor-not-allowed shadow-md rounded-md duration-200">
             { props.content }
         </button>
